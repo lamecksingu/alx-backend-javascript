92a59469d0084d311cd05303be72ba391442c45b
lamecksingu <singulameck9619@gmail.com>
2024-10-09 20:12:51 +0300

    Task 4 implementation

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,22 @@
+namespace Subjects {
+  export interface Teacher {
+    firstName: string;
+    lastName: string;
+    experienceTeachingC?: number;
+    experienceTeachingReact?: number;
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Java';
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher.experienceTeachingJava) {
+        return 'No available teacher';
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
