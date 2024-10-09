2a9ae0e61bde7e61e9f24118fb26c3b5596cf6d2
lamecksingu <singulameck9619@gmail.com>
2024-10-09 19:55:02 +0300

    Task 3 implementation

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,25 @@
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva'
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+
+const updatedRow: RowElement = {
+  ...row,
+  age: 23
+};
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
+
+const obj = {firstName: "Guillaume", lastName: "Salva"};
+CRUD.insertRow(obj);
+
+const updatedRow: RowElement = { firstName: "Guillaume", lastName: "Salva", age: 23 };
+CRUD.updateRow(newRowID, updatedRow);
+
+CRUD.deleteRow(125);
